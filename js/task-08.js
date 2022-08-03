0f4d41241cdc5cd4c0ad46a52ca31c7f61cbfaac
Oleksii <97940139+Tsakhlo@users.noreply.github.com>
2022-08-03 22:02:49 +0300

    +

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', loginForm);
 
+function loginForm(event) {
+    event.preventDefault()
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+         if (email === "" || password === "") {
+        alert("Not all fields are filled");
+    } else {
+        const formData = {
+            email,
+            password,
+        };
+             console.log(formData);
+    }
+    event.target.reset();   
+}
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,7 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+function addBoxes () {
+  const click = document.querySelector()
+}
+console.log(addBoxes);
